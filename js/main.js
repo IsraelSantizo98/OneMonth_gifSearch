fed01466d1ae89f88cf034a0750cb4395a172c4d
Israel Santizo <joseisraelss.jiss@gmail.com>
2020-12-27 22:06:31 -0600

    Se crea funcion para colocar en DOM lo que se escribe en el input

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 //Gran de value in the input
-var input = document.querySelector('input').value;
-console.log(input)
+document.querySelector('.js-go').addEventListener('click', function(){
+    //alert('test')
+    var input = document.querySelector('input').value;
+    console.log(input);
+    pushToDom(input);
+});
+function pushToDom(input){
+    var container = document.querySelector('.js-container');
+    container.innerHTML = input;
+};
