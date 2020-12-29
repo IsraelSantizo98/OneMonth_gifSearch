fad9f4487c260fe876854ac27955ca6292d041a6
Israel Santizo <joseisraelss.jiss@gmail.com>
2020-12-29 13:19:00 -0600

    Se crea reconocimiento del teclado para el buscador con la tecla enter

--- js/main.js
+++ js/main.js
@@ -5,6 +5,28 @@ document.querySelector('.js-go').addEventListener('click', function(){
     console.log(input);
     pushToDom(input);
 });
+//Reconcer el click del teclado
+document.querySelector('.js-userinput').addEventListener('keyup', function(e){
+    //The e in the function es the data for de event
+    console.log(e);
+    var input = document.querySelector('input').value;
+    console.log(input);
+    //The number 13 is the code for enter in the keyboard
+    //The keycode in js not compatible for all browsers
+    if(e.which === 13){
+        pushToDom(input);
+    }else{
+        //alert('N era el enter');
+    }
+});
+/* Para escribir directamente el DOM
+document.querySelector('.js-userinput').addEventListener('keyup', function(){
+    //alert('test')
+    var input = document.querySelector('input').value;
+    console.log(input);
+    pushToDom(input);
+});
+*/
 function pushToDom(input){
     var container = document.querySelector('.js-container');
     container.innerHTML = input;
